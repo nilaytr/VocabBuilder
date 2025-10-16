592abd70b83ba5bb0ff0e349f66f9f0ccd961f77
Nilay Türker <nilaytr89@gmail.com>
2025-10-16 23:17:17 +0300

    app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,1 +1,11 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+import { Loader } from "../Loader/Loader";
+import { useDispatch } from "react-redux";
 
+const LoginPage = lazy(() => import('../../pages/LoginPage/LoginPage'));
+const RegisterPage = lazy(() => import('../../pages/RegisterPage/RegisterPage'));
+const NotFoundPage = lazy(() => import('../../pages/NotFoundPage/NotFoundPage'));
+const DictionaryPage = lazy(() => import('../../pages/DictionaryPage/DictionaryPage'));
+const RecommendPage = lazy(() => import('../../pages/RecommendPage/RecommendPage'));
+const TrainingPage = lazy(() => import('../../pages/TrainingPage/TrainingPage'));
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { Provider } from 'react-redux';
+import { persistor, store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App/App";
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
