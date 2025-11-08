4bc37ff3e9a620cb03cc8f0405add9bff8ace183
Nilay Türker <nilaytr89@gmail.com>
2025-11-08 18:29:38 +0300

    Add DashBoard component and update imports

--- src/pages/RecommendPage/RecommendPage.jsx
+++ src/pages/RecommendPage/RecommendPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectWords, selectTotalPages } from "../../redux/word/selectors";
 import { fetchAllWords, addWord } from "../../redux/word/operations";
-import Dashboard from "../../components/Dashboard/Dashboard";
+import Dashboard from "../../components/Dashboard/DashBoard";
 import WordsTable from "../../components/WordsTable/WordsTable";
 import WordsPagination from "../../components/WordsPagination/WordsPagination";
 
